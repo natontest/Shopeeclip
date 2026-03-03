18cd64711f97b5bd72bdeb702e3367b2defd12ef
natontest <shoppevideo7@gmail.com>
2026-03-03 16:57:15 -0300

    Update server.js
    Fix public path

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // ── UTILITÁRIOS ──────────────────────────────────────────
 
